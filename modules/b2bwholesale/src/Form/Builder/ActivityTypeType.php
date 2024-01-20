e6368b514dd7b8551b581e003493559f96b864f2
Tomo Stjepovic <tomo.stjepoviv@gmail.com>
2024-01-20 22:13:10 +0100

    Added all form types, CRUD fixed

--- modules/b2bwholesale/src/Form/Builder/ActivityTypeType.php
+++ modules/b2bwholesale/src/Form/Builder/ActivityTypeType.php
@@ -9,55 +9,21 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ActivityTypeType extends TranslatorAwareType
-{/**
- * @var bool
- */
-    private $isMultistoreEnabled;
-
-    /**
-     * @param TranslatorInterface $translator
-     * @param array $locales
-     * @param bool $isMultistoreEnabled
-     */
-    /*public function __construct(
-        TranslatorInterface $translator,
-        array $locales,
-        bool $isMultistoreEnabled
-    ) {
-        parent::__construct($translator, $locales, $isMultistoreEnabled);
-
-        $this->isMultistoreEnabled = $isMultistoreEnabled;
-    }*/
-
+{
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('id', HiddenType::class)
             ->add('name', TranslatableType::class, array(
+                'label' => $this->trans('Name', 'Admin.Global'),
                 "attr" => array(
                     'label' => $this->trans('Name', 'Admin.Global'),
                     "placeholder" => "Name",
                     'type' => TextType::class,
                 )
             ));
-        if ($this->isMultistoreEnabled) {
-            $builder->add('shop_association', ShopChoiceTreeType::class, [
-                'label' => $this->trans('Shop association', 'Admin.Global'),
-                'required' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => $this->trans(
-                            'You have to select at least one shop to associate this item with',
-                            'Admin.Notifications.Error'
-                        ),
-                    ]),
-                ],
-            ]);
-        }
 
         $builder->add('save', SubmitType::class);
     }
